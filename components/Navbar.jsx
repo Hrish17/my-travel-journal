d6b68aea323d740a4ec380bddb230acd303e58bb
Hrish17 <102007737+Hrish17@users.noreply.github.com>
2023-08-20 15:11:47 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar(){
     return(
         <nav>
             <div className="nav-items">
-                <img src="images/nav-logo.png" className="nav-logo"/>
+                <img src="https://imgtr.ee/images/2023/08/20/f8935ad46ded3e234394ab1626fdc9ca.png" className="nav-logo"/>
                 <p className="nav-title">my travel journal.</p>
             </div>
         </nav>
