32f803067270c6ab4d6d9ed9c64dd81991d4914f
Hrish17 <102007737+Hrish17@users.noreply.github.com>
2023-08-20 15:12:28 +0530

    Update Card.jsx

--- components/Card.jsx
+++ components/Card.jsx
@@ -6,7 +6,7 @@ export default function Card(prop){
             <img src={prop.img} className="card-img" />
             <div className="card-info">
                 <div className="card-location">
-                    <img src="images/pin.png" />
+                    <img src="https://imgtr.ee/images/2023/08/20/6b4c6daf12452f203c18951844db6a7c.png" />
                     <p>{prop.location}</p>
                     <a href={prop.link} target="_blank">View on Google Maps</a>
                 </div>
